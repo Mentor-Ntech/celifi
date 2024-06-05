8476a704e00f6b38f91cce6ddc579822c52eaec0
RonexLemon <ronexondimu@gmail.com>
2024-06-05 18:18:07 +0300

    Tokens

---
+++ celifi/src/Utils/Tokens.ts
@@ -0,0 +1,50 @@
+
+
+const cUSD={
+"name": "Celo Dollar",
+"symbol": "cUSD",
+"address": "0x765de816845861e75a25fca122bb6898b8b1282a",
+"decimals": 18,
+"image": "https://assets.coingecko.com/coins/images/10366/large"
+}
+
+const cEUR={
+"name": "Celo Europe",
+"symbol": "cEUR",
+"address": "0xd8763cba276a3738e6de85b4b3bf5fded6d6ca73",
+"decimals": 18,
+"image": "https://assets.coingecko.com/coins/images/10366/large"
+}
+const cREAL={
+"name": "Celo Brazil",
+"symbol": "cREAL:",
+"address": "00xe8537a3d056DA446677B9E9d6c5dB704EaAb4787",
+"decimals": 18,
+"image": "https://assets.coingecko.com/coins/images/10366/large"
+}
+const CELO={
+"name": "Celo",
+"symbol": "CELO:",
+"address": "0x471EcE3750Da237f93B8E339c536989b8978a438",
+"decimals": 18,
+"image": "https://assets.coingecko.com/coins/images/10366/large"
+}
+
+
+//update the ckes contract
+const cKES={
+    "name": "Celo Kenya",
+    "symbol": "cKes:",
+    "address": "0x471EcE3750Da237f93B8E339c536989b8978a438",
+    "decimals": 18,
+    "image": "https://assets.coingecko.com/coins/images/10366/large"
+    }
+
+    export  const Tokens =[
+        cUSD,
+        cEUR,
+        cREAL,
+        CELO,
+        cKES   
+    
+    ]
