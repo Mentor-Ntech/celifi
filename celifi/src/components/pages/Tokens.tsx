f3c3e265a14cbd32b3c41c602091efc273614343
RonexLemon <ronexondimu@gmail.com>
2024-06-11 23:42:21 +0300

    tokens table

--- celifi/src/components/pages/Tokens.tsx
+++ celifi/src/components/pages/Tokens.tsx
@@ -1,4 +1,4 @@
-
+"use client"
 import React, { useState } from "react"
 import { Input } from "../ui/input"
 import { SearchIcon } from "../icons/Search"
@@ -7,11 +7,14 @@ import { CelifiIcon } from "../icons/CelifiLogo"
 import { HeroIcon } from "../icons/Hero"
 import { SendRequest } from "../SendRequest"
 import { Chart } from "../chart"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { TokensTable } from "../TokensTable"
+
 
 export function Tokens() {
   
   return (
-      <div className="w-screen min-h-screen relative">
+      <div className="w-screen min-h-screen ">
           
           <div className="flex justify-between md:justify-end items-center w-full pr-4  md:pr-32  border-b-2 border-Celifi-Light-Green">
               <div className="block md:hidden "> <Button className="bg-transparent   hover:bg-transparent"><CelifiIcon/></Button></div>
@@ -20,28 +23,30 @@ export function Tokens() {
 
 
           </div>
-          <div className="w-full h-1/2 flex justify-center mt-5   items-center">
-            <Chart/>
-              
+          <div className="w-full h-1/2 flex justify-center mt-5   items-center ">
+            <Chart/>             
 
           </div>
 
-          <div className="w-full p-8 flex flex-col text-Celifi-Yellow justify-center items-center   ">
-              <h1 >The multi-utility dashboard</h1>
-              <h1>that accelerates your Defi Assessment</h1>
-
+          <div className="w-full  flex flex-col text-Celifi-Yellow justify-between  md:justify-center items-center   ">
+          <Tabs defaultValue="account" className="md:w-3/4  w-full">
+  <TabsList className="flex justify-between items-center bg-transparent ">
+    <TabsTrigger className="" value="tokens">Tokens</TabsTrigger>
+    <TabsTrigger value="nft">NFT</TabsTrigger>
+    <TabsTrigger value="defi">Defi</TabsTrigger>
+    <TabsTrigger value="performance">Performance</TabsTrigger>
+   
+  </TabsList>
+  <TabsContent value="tokens">
+    <TokensTable/>
+  </TabsContent>
+  <TabsContent value="nft">Change your NFT here.</TabsContent>
+  <TabsContent value="defi">Change your DEFI here.</TabsContent>
+  <TabsContent value="performance">Change your Performance here.</TabsContent>
+</Tabs>
 
           </div>
-          <div className="w-full p-4 ">
-          <Button className="rounded border-none w-full  md:w-1/4  bg-Celifi-Light-Green   hover:bg-Celifi-Light-Green  " ><SearchIcon/> <Input type="email" className="bg-transparent  border-none outline-none focus-visible:border-none focus-visible:outline-none focus-visible:ring-0 ring-offset-0 ring-0" placeholder="Search Address"/></Button>
-          <Button   variant="link" className=" text-[#C5C83E] bg-clip-text">Send us a feature request</Button>
-
-          </div>
-
-          <div className="absolute bottom-0 left-0 w-full flex justify-center mb-4">
-              <HeroIcon />
-
-          </div>
+        
         
       </div>
 
