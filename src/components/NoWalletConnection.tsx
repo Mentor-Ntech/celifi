13438928b7425c83c8a8ff3186256e204690ce29
RonexLemon <ronexondimu@gmail.com>
2024-06-22 14:43:08 +0300

    address check

--- src/components/NoWalletConnection.tsx
+++ src/components/NoWalletConnection.tsx
@@ -1,21 +1,36 @@
 import React from "react"
 
 import  {Card,CardContent,CardDescription,CardHeader,CardFooter,CardTitle} from "./ui/card"
+import Image from "next/image"
 
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+  import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+  } from "@/components/ui/alert"
+import { RocketIcon } from "lucide-react"
+import { NetWorkDangerIcon } from "./icons/netWorkDanger"
+import { CelifiIcon } from "./icons/CelifiLogo"
 const NoWallet =()=>{
     return(
-        <Card className="w-1/2 h-1/4">
-            <CardContent>
-                <CardHeader>
-                    Please Connect a Wallet
-                </CardHeader>
-
-
-            </CardContent>
-
-
-        </Card>
-
+        <Alert className="bg-Celifi-Slate-Yellow">
+      <RocketIcon className="h-4 w-4" />
+      <AlertTitle>Connect Your Wallet!</AlertTitle>
+      <AlertDescription className="flex justify-center items-center ">
+       <NetWorkDangerIcon />
+      </AlertDescription>
+    </Alert>
     )
 
 }
