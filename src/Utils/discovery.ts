ab18943dfa9d488ba5525310ca4b1f3972ebbf1f
RonexLemon <ronexondimu@gmail.com>
2024-07-28 23:52:04 +0300

    discovery

---
+++ src/Utils/discovery.ts
@@ -0,0 +1,22 @@
+//const { Mento } = require("@mento-protocol/mento-sdk");
+//const { providers } = require("ethers");
+
+interface TradePairs{
+    
+}
+
+
+import { providers } from "ethers";
+import {Mento} from "@mento-protocol/mento-sdk"
+import { MainentRPC,provider } from "@/components/token/TokensData";
+
+export const getAllTokenTradePairs = async()=>{
+    const mento = await Mento.create(provider);
+
+    const pairs = await mento.getTradeablePairs();
+    console.log(`Found ${pairs.length} tradeable pairs:`);
+    return pairs;
+    
+
+}
+
